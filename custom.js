28cbc07cab40babd3e00bc1fd4e6d03e17794615
Betzabeth Narvaez <betzynarv@gmail.com>
2020-12-18 05:32:09 -0600

    experiment with variables

--- custom.js
+++ custom.js
@@ -1,1 +1,7 @@
 // NARVAEZ BETZABETH 2018106117
+
+//get values of inputs and put into variables
+var apple_q = document.getElementById("apple_q").value;
+var orange_q = document.getElementById("orange_q").value;
+var banana_q = document.getElementById("banana_q").value;
+var watermelon_q = document.getElementById("watermelon_q").value;
