de4803ca5db94c3f8d6116a2d06e1115425ea72e
Betzabeth Narvaez <betzynarv@gmail.com>
2020-12-18 06:22:11 -0600

    try local storage for variables

--- custom.js
+++ custom.js
@@ -5,15 +5,15 @@
 var f_submit = document.getElementById("first_submit")
 
 f_submit.onclick = function () {
-    var apple_qu = document.getElementById("apple_q").value;
-    var orange_qu = document.getElementById("orange_q").value;
-    var banana_qu = document.getElementById("banana_q").value;
-    var watermelon_qu = document.getElementById("watermelon_q").value;
+    localStorage["apple_qu"] = document.getElementById("apple_q").value;
+    localStorage["orange_qu"] = document.getElementById("orange_q").value;
+    localStorage["banana_qu"] = document.getElementById("banana_q").value;
+    localStorage["watermelon_qu"] = document.getElementById("watermelon_q").value;
 
-    document.getElementById("o_apple").innerText = apple_qu;
-    document.getElementById("o_orange").innerText = orange_qu;
-    document.getElementById("o_banana").innerText = banana_qu;
-    document.getElementById("o_watermelon").innerText = watermelon_qu;
+    document.getElementById("o_apple").innerText = localStorage["apple_qu"];
+    document.getElementById("o_orange").innerText = localStorage["orange_qu"];
+    document.getElementById("o_banana").innerText = localStorage["banana_qu"];
+    document.getElementById("o_watermelon").innerText = localStorage["watermelon_qu"];
 
 }
 
