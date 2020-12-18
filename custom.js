4d0226f45bc8e4fe914a185a9806e2e9ce0c525a
Betzabeth Narvaez <betzynarv@gmail.com>
2020-12-18 05:58:42 -0600

    mess with variables

--- custom.js
+++ custom.js
@@ -1,7 +1,20 @@
 // NARVAEZ BETZABETH 2018106117
 
 //get values of inputs and put into variables
-var apple_q = document.getElementById("apple_q").value;
-var orange_q = document.getElementById("orange_q").value;
-var banana_q = document.getElementById("banana_q").value;
-var watermelon_q = document.getElementById("watermelon_q").value;
+
+var f_submit = document.getElementById("first_submit")
+
+f_submit.onclick = function () {
+    var apple_qu = document.getElementById("apple_q").value;
+    var orange_qu = document.getElementById("orange_q").value;
+    var banana_qu = document.getElementById("banana_q").value;
+    var watermelon_qu = document.getElementById("watermelon_q").value;
+
+    document.getElementById("o_apple").innerText = apple_qu;
+    document.getElementById("o_orange").innerText = orange_qu;
+    document.getElementById("o_banana").innerText = banana_qu;
+    document.getElementById("o_watermelon").innerText = watermelon_qu;
+
+}
+
+
