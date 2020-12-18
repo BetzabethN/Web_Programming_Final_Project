514690e00edbc2f248ff7ba11cebe8b70adeb5bd
Betzabeth Narvaez <betzynarv@gmail.com>
2020-12-18 06:31:31 -0600

    mess with local storage variables

--- custom.js
+++ custom.js
@@ -2,19 +2,19 @@
 
 //get values of inputs and put into variables
 
-var f_submit = document.getElementById("first_submit")
+//var f_submit = document.getElementById("first_submit")
 
-f_submit.onclick = function () {
-    localStorage["apple_qu"] = document.getElementById("apple_q").value;
-    localStorage["orange_qu"] = document.getElementById("orange_q").value;
-    localStorage["banana_qu"] = document.getElementById("banana_q").value;
-    localStorage["watermelon_qu"] = document.getElementById("watermelon_q").value;
+//f_submit.onclick = function () {
+   // localStorage["apple_qu"] = document.getElementById("apple_q").value;
+   // localStorage["orange_qu"] = document.getElementById("orange_q").value;
+   // localStorage["banana_qu"] = document.getElementById("banana_q").value;
+   // localStorage["watermelon_qu"] = document.getElementById("watermelon_q").value;
 
-    document.getElementById("o_apple").innerText = localStorage["apple_qu"];
-    document.getElementById("o_orange").innerText = localStorage["orange_qu"];
-    document.getElementById("o_banana").innerText = localStorage["banana_qu"];
-    document.getElementById("o_watermelon").innerText = localStorage["watermelon_qu"];
+   // document.getElementById("o_apple").innerText = localStorage["apple_qu"];
+   // document.getElementById("o_orange").innerText = localStorage["orange_qu"];
+   // document.getElementById("o_banana").innerText = localStorage["banana_qu"];
+   // document.getElementById("o_watermelon").innerText = localStorage["watermelon_qu"];
 
-}
+//}
 
 
